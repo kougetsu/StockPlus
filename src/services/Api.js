5dc090e885fbf196986102503b80823ebb94d5d8
kougetsu <mattia.dc091@gmail.com>
2022-01-25 11:21:02 +0900

    fix switch api key to .env file

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const Api = axios.create({
   baseURL: `https://financialmodelingprep.com/api/`,
   params: {
-    apikey: '05453aa5416c0617a356443dec29d03d',
+    apikey: process.env.REACT_APP_API_KEY,
   },
 })
 
